05c7b15c56901ae0d99f3a652749aeb4e1708b56
bonvinsamuel-maker <bonvin.samuel@netplus.ch>
2026-05-27 11:46:26 +0200

    Class

--- src/main/java/BlackJackMain/Card.java
+++ src/main/java/BlackJackMain/Card.java
@@ -1,15 +1,22 @@
 package BlackJackMain;
 
 public class Card {
-    String suit;
+    String couleur;
     String name;
     int value;
     String imagePath;
     public Card(String suit, String name, int value) {
-        this.suit = suit;
+        this.couleur = suit;
         this.name = name;
         this.value = value;
         this.imagePath = imagePath;
+    }public String getCouleur() {
+        return this.couleur;
+    }public String getName() {
+        return this.name;
+    }public int getValue() {
+        return this.value;
+    }public String getImagePath() {
+        return this.imagePath;
     }
-
 }
--- src/main/java/BlackJackMain/Deck.java
+++ src/main/java/BlackJackMain/Deck.java
@@ -1,4 +1,10 @@
 package BlackJackMain;
 
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> deck=new ArrayList<>(); //je crée mon Array list que je vais remplir ensuite
+    String[] couleurs = {"Coeur","Carreau";"Trèfle","Pique"};
+    String[] names = {"2","3","4","5","6","7","8","9","10","Valet","Reine","Roi","As"};
+    int[] values= {2,3,4,5,6,7,8,9,10,10,10,10,11};
 }
