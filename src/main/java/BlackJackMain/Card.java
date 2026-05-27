a07ee3aeddcd4d121dec3555d4855df5a55a1850
bonvinsamuel-maker <bonvin.samuel@netplus.ch>
2026-05-27 11:24:40 +0200

    Class

--- src/main/java/BlackJackMain/Card.java
+++ src/main/java/BlackJackMain/Card.java
@@ -1,4 +1,15 @@
 package BlackJackMain;
 
 public class Card {
+    String suit;
+    String name;
+    int value;
+    String imagePath;
+    public Card(String suit, String name, int value) {
+        this.suit = suit;
+        this.name = name;
+        this.value = value;
+        this.imagePath = imagePath;
+    }
+
 }
--- src/main/java/BlackJackMain/Main.java
+++ src/main/java/BlackJackMain/Main.java
@@ -1,4 +1,5 @@
 package BlackJackMain;
 
 public class Main {
+
 }
